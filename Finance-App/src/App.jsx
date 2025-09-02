5602775b9ab87583567130247d2640eaf47e403b
perrychambersjr <perrychambers0@gmail.com>
2025-09-02 16:41:04 -0400

    Created initial scaffolding with react and tailwind setup

---
+++ Finance-App/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+export default function App() {
+
+  return (
+<div className="p-6 bg-beige-100 text-grey-900 rounded-lg shadow">
+  <h1 className="heading-xl text-green">Budget Overview</h1>
+  <p className="subheading-sm text-grey-500">Track your monthly expenses</p>
+  <h2 className="heading-lg text-cyan">Phasellus ultrices nulla quis nibh</h2>
+  <p className="body-xs text-grey-900">
+    Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula
+    vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.
+  </p>
+  <p className="body-sm text-navyGrey">
+    Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.
+  </p>
+</div>
+
+  )
+}
